436c43c75fe08779510d8ddf71a0b4107423c701
ZachPetersen <zjpetersen12@gmail.com>
2021-09-08 23:05:04 -0500

    Bulk upload initial implementation

--- client/src/SetColor.js
+++ client/src/SetColor.js
@@ -8,8 +8,8 @@ class SetColor extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { tileId: '', color: '', stackId: null, imageDisplayed: false, hex: '' };
-    this.handleChangeColor = this.handleChangeColor.bind(this);
+    this.state = { tileId: '', color: '', stackId: null, imageDisplayed: false, hex: '', isBulkMode: false, fileNames: [], tileIds: '', tileIdsPending: '' };
+    // this.handleChangeColor = this.handleChangeColor.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.displayColor = this.displayColor.bind(this);
     this.updateColor = this.updateColor.bind(this);
@@ -21,15 +21,16 @@ class SetColor extends React.Component {
   }
 
 
-  handleChangeColor(event) {
-    this.setState({ color: event.target.value });
-  }
+  // handleChangeColor(event) {
+  //   this.setState({ color: event.target.value });
+  // }
 
   handleSubmit(event) {
     const { drizzle, drizzleState } = this.props;
     const contract = drizzle.contracts.CryptoCanvas;
 
-    let hex = this.bytesToHex(this.state.color);
+    let hex = this.bytesToHex(this.state.color[0]);
+    console.log(hex);
     const stackId = contract.methods["setColorBytes"].cacheSend(this.props.tileId, hex, {
       from: drizzleState.accounts[0]
     });
@@ -80,6 +81,76 @@ class SetColor extends React.Component {
     return result;
   }
 
+  updateColorBulk = () => {
+    if (this.state.color === '' || !this.state.isBulkMode) {
+      return;
+    }
+    return <div className="center">
+      <form onSubmit={(event) => this.handleUpdateColorBulkSubmit(event)}>
+        <label> Submit artwork. Specify tiles as a comma separated list:
+                <input
+            value={this.state.amount}
+            onChange={(event) => this.handleUpdateColorBulkChange(event)} />
+        </label>
+        <input className="margin" type="submit" value="Submit" />
+      </form>
+    </div>
+
+  }
+
+  handleUpdateColorBulkChange(event) {
+    this.setState({tileIdsPending: event.target.value });
+  }
+
+  handleUpdateColorBulkSubmit(event) {
+      event.preventDefault();
+    if (!this.state.isBulkMode || this.state.fileNames < 2) {
+      //TODO should return error message to indicate needs more files.
+      console.log("should return error message to indicate needs more files");
+      return;
+    }
+      let tileIdsPending = this.state.tileIdsPending;
+      if (typeof tileIdsPending !== 'string') {
+        console.log("Input not a string");
+        return;
+      }
+
+      let tileIdsArray = tileIdsPending.split(',');
+      if (tileIdsArray.length !== this.state.fileNames.length) {
+        console.log("File length and tileId lengths much match");
+        return;
+      }
+      let tileIds = [];
+      for (let i = 0; i < tileIdsArray.length; i++) {
+        let id = parseInt(tileIdsArray[i]);
+        if (!Number.isInteger(id)) {
+          console.log("Input must all be integers");
+          return;
+        }
+        tileIds.push(id);
+      }
+
+      let hexArray = [];
+      for (let i = 0; i < this.state.color.length; i++) {
+        let hex = this.bytesToHex(this.state.color[i]);
+        hexArray.push(hex);
+      }
+
+
+      //Submit bulk colors to contract
+    const { drizzle, drizzleState } = this.props;
+    const contract = drizzle.contracts.CryptoCanvas;
+
+    const stackId = contract.methods["setColorBytesBulk"].cacheSend(tileIds, hexArray, {
+      from: drizzleState.accounts[0]
+    });
+
+    //TODO: Indicate that upload succeeded and update canvas.
+
+    // this.setState({ stackId, hex });
+    
+  }
+
   dataUrlFromInt8Array(int8array) {
     let uint = Uint8Array.from(int8array);
     let imgSrc = 'data:image/png;base64,';
@@ -87,12 +158,56 @@ class SetColor extends React.Component {
     return imgSrc;
   }
 
+  displayBulkColor = () => {
+    if (this.state.color === '' || !this.state.isBulkMode) {
+      return;
+    }
+    let bulkError;
+    if (this.state.bulkError) {
+      bulkError = <div style={{color:"red"}}>Error, width and height must both be 16 px, please reupload another image.</div>
+    }
+    let count = this.state.color.length;
+    let fileRows = [];
+    let fileNames = this.state.fileNames;
+    for (let i = 0; i < fileNames.length; i++) {
+      let row = <div key={i}>Uploaded file: {fileNames[i]}, position: {i}</div>;
+      fileRows.push(row);
+
+    }
+    return <div>
+            <p>Uploaded {count} artworks</p>
+            {fileRows}
+            {bulkError}
+          </div>
+  }
+
+  validateColor = (expectedColor) => {
+    if (!expectedColor) {
+      return;
+    }
+
+    let imgSrc = this.dataUrlFromInt8Array(expectedColor);
+
+    let i = new Image();
+    i.onload = () => {
+      //If invalid size, then display message and remove
+      if (i.naturalWidth !== 16 || i.naturalHeight !== 16) {
+        this.state.color.pop();
+        this.state.fileNames.pop();
+        this.setState({bulkError: true})
+      } 
+    }
+    i.src = imgSrc;
+    return;
+
+  }
+
   displayColor = () => {
     if (this.state.color === '') {
       return;
     }
 
-    let imgSrc = this.dataUrlFromInt8Array(this.state.color);
+    let imgSrc = this.dataUrlFromInt8Array(this.state.color[0]);
 
     let i = new Image();
     //TODO would be better to handle this with normal react jsx
@@ -122,7 +237,7 @@ class SetColor extends React.Component {
   }
 
   readFile = async (e) => {
-    e.preventDefault()
+    // e.preventDefault()
     const reader = new FileReader()
     const fileList = e.target.files;
     //Catch error if trying to cancel when uploading a second file
@@ -134,13 +249,49 @@ class SetColor extends React.Component {
       const buffer = (e.target.result)
       let expectedColor = new Int8Array(buffer);
 
-      this.setState({imageDisplayed: false});
-      this.setState({ color: expectedColor});
-    this.displayColor();
+      if (this.state.isBulkMode) {
+        let newColor = this.state.color;
+        if (newColor !== '') {
+          newColor.push(expectedColor);
+        } else {
+          newColor = [expectedColor];
+        }
+        this.setState({ imageDisplayed: false });
+        this.setState({ color: newColor, bulkError: false });
+        this.validateColor(expectedColor);
+      } else {
+        this.setState({ imageDisplayed: false });
+        this.setState({ color: [expectedColor] });
+        this.displayColor();
+      }
     };
     // reader.readAsDataURL(e.target.files[0])
     let file = e.target.files[0];
+    let newFileNames = this.state.fileNames;
+    newFileNames.push(file.name);
+    this.setState({fileNames: newFileNames});
     reader.readAsArrayBuffer(file);
+    let uploadedFiles = document.getElementById("file-selector");
+    if (uploadedFiles) {
+      uploadedFiles.value = "";
+    }
+
+  }
+
+
+  toggleBulkMode(event) {
+    this.setState({isBulkMode: !this.state.isBulkMode});
+  }
+
+  colorInput() {
+    let msg = !this.state.isBulkMode ? "Toggle To Bulk Update Mode" : "Toggle To Single Update Mode";
+    let bulkWarning = !this.state.isBulkMode ? "" : "Warning, the bulk upload feature is still experimental!";
+    return <div>
+      <button className="small" type="button" onClick={() => this.toggleBulkMode()}>{msg}</button>
+      <br></br>
+      <input type="file" id="file-selector" onChange={(e) => this.readFile(e)} />
+      <p style={{ color: "red" }}>{bulkWarning}</p>
+    </div>
 
   }
 
@@ -171,13 +322,19 @@ class SetColor extends React.Component {
         <div>
           <h2>Upload artwork</h2>
           <p>Please ensure it is a png and the size is 16x16px</p>
-          <input type="file" id="file-selector" onChange={(e) => this.readFile(e)} />
+          <div>
+            {this.colorInput()}
+          </div>
         </div>
         <div id="pic">
           {/* {this.displayColor()} */}
         </div>
         <div>
           {this.updateColor()}
+        </div>
+        <div>
+          {this.displayBulkColor()}
+          {this.updateColorBulk()}
         </div>
       </div>
     );
