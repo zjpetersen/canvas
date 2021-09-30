cf232cff4ca3c27ed2b3d74338ddf1e46b239ed3
ZachPetersen <zjpetersen12@gmail.com>
2021-09-29 22:36:44 -0500

    Fix links and address

--- client/src/Home.js
+++ client/src/Home.js
@@ -12,10 +12,9 @@ class Home extends React.Component {
         
         </div>
         <div className="column middle">
-          <p>**Note EtherCanvas is currently deployed on the Rinkeby testnet.  Stay tuned for mainnet release!</p>
 
           <h2>How does it work?</h2>
-          <p>EtherCanvas is a Dapp running on the ethereum blockchain.  
+          <p>EtherCanvas is a Dapp running on the Ethereum Polygon blockchain.  
             It is a digital canvas made up of 7056 non-fungible tokens (tiles).  Tile owners are able to create their own artwork and add their updated tile to the <a href="/">Canvas</a>.
             All of the tiles are displayed next to each other to make the overall art piece.
             </p>
@@ -26,7 +25,7 @@ class Home extends React.Component {
 
           <h4>Trading tiles</h4>
           <p>Like other NFTs, you can buy and sell your NFT (the tile) whenever you want.  
-            Trading can be done through any NFT marketplace, although <a href="https://testnets.opensea.io/collection/ethercanvas">OpenSea</a> is where they will be initially listed. 
+            Trading can be done through any NFT marketplace, although <a href="https://opensea.io/collection/ether-canvas">OpenSea</a> is where they will be initially listed. 
             If you are interested in purchasing a tile, checkout the 'How Can I purchase a tile?' section below.
             </p>
 
@@ -45,10 +44,13 @@ class Home extends React.Component {
             You can think of the tile as a plot of land.  As long as you own the land, you are free to put whatever you want on it.</p>
 
           <h2>How can I purchase a tile?</h2>
+            <p>In order to purchase an NFT on Polygon you must first bridge your Ethereum.  Below are the high level steps.  
+              OpenSea also has a <a href="https://support.opensea.io/hc/en-us/articles/1500012881642-How-do-I-transfer-ETH-from-Ethereum-to-Polygon-">detailed tutorial</a>.</p>
             <ol>
               <li>Install the Chrome extension <a href="https://metamask.io/">MetaMask</a>.</li>
               <li>Send some Ethereum to your MetaMask wallet.  For example, from your Coinbase wallet.</li>
-              <li>Navigate to the <a href="https://testnets.opensea.io/collection/ethercanvas">OpenSea Testnet collection</a> and pick a tile you're interested in.</li>
+              <li><a href="https://support.opensea.io/hc/en-us/articles/1500012881642-How-do-I-transfer-ETH-from-Ethereum-to-Polygon-">Bridge your Ethereum to Polygon</a></li>
+              <li>Navigate to the <a href="https://opensea.io/collection/ether-canvas">OpenSea collection</a> and pick a tile you're interested in.</li>
               <li>Click the 'Buy Now' button.  Congratulations! You now have your own NFT!</li>
               <li>Check out the <a href="https://support.opensea.io/hc/en-us/articles/360063518033-How-do-I-buy-fixed-price-NFTs-">OpenSea documentation</a> for detailed instructions.</li>
             </ol>
@@ -56,7 +58,7 @@ class Home extends React.Component {
           <h2>Other technical details</h2>
             <ul>
               <li>There are 7056 total tiles (63 rows and 112 columns).  Each tile is 16x16 pixels wide.  This means the Canvas is 1008x1794 pixels, which fits on a 1080p screen with just a bit to spare. </li>
-              <li>One might wonder what the gas fees are like for updating artwork.  To store even just 256 pixels worth of data on the blockchain would be very expensive.  To get around this, the art data is emitted as an event.  This makes it much cheaper, while still being fully decentralized and traceable.  </li>
+              <li>One might wonder what the gas fees are like for updating artwork.  To avoid excessive gas fees, the contract is deployed on the <a href="https://polygon.technology/">Polygon</a> sidechain.  This makes fees much, much lower.  Another way fees are reduced is by having the art data emitted as an event.  Both of these make it much cheaper, while still being fully decentralized and traceable.  </li>
             </ul>
 
         </div>
@@ -79,7 +81,7 @@ render() {
             <a className="fa fa-github" href="https://github.com/zjpetersen/canvas-contracts"></a>
           </div>
           <div className="column middle">
-            Polygon: 0xb8120f116f88942ACF50B1d797977FefDaB206e0
+            Polygon: 0xbd21CF77E72b2f1cbC60F40dD36ed843Fb793dd2
             <br></br>
             <br></br>
             Testnet: 0x59b6722596e25a8721AB26664a2AfbF1C7D90818
